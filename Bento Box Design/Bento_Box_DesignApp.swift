05f2b95df2db9842eba37bf067667818ccee6454
Nina Zhou <nzhou@lcs.on.ca>
2023-10-27 08:43:50 -0400

    Initial Commit

---
+++ Bento Box Design/Bento_Box_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bento_Box_DesignApp.swift
+//  Bento Box Design
+//
+//  Created by Shuyu Zhou on 2023-10-27.
+//
+
+import SwiftUI
+
+@main
+struct Bento_Box_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bento Box Design/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bento Box Design
+//
+//  Created by Shuyu Zhou on 2023-10-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
