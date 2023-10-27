696a5ca22fcbdd664eac0bfc9b651c5e971e6000
Nina Zhou <nzhou@lcs.on.ca>
2023-10-27 16:56:46 -0400

    basic layout & first 2 rectangles

--- Bento Box Design/ContentView.swift
+++ Bento Box Design/ContentView.swift
@@ -9,12 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "tray.circle")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        HStack{
+            VStack{
+                HStack{
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                        VStack {
+                            Image("Thunderbollt")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 30, height: 40, alignment: .center)
+                            Text("Thunderbolt 4")
+                                .font(.caption)
+                                .frame(alignment: .bottom)
+                                .foregroundStyle(.black)
+                        }
+                    }
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 20)
+                        VStack{
+                            Text("Up to")
+                                .foregroundStyle(.black)
+                                .font(.caption)
+                            Spacer()
+                            Text("20%")
+                                .foregroundStyle(.black)
+                                .font(.largeTitle)
+                                .bold()
+                            Spacer()
+                            Text("faster CPU")
+                                .foregroundStyle(.black)
+                                .font(.caption)
+                        }
+                    }
+                }
+                RoundedRectangle(cornerRadius: 20)
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                    RoundedRectangle(cornerRadius: 20)
+                }
+                RoundedRectangle(cornerRadius: 20)
+            }
+            .frame(maxWidth: 215)
+            VStack{
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                    RoundedRectangle(cornerRadius: 20)
+                }
+                RoundedRectangle(cornerRadius: 20)
+                RoundedRectangle(cornerRadius: 20)
+            }
+            .frame(maxWidth: 270)
+            VStack{
+                RoundedRectangle(cornerRadius: 20)
+                HStack{
+                    RoundedRectangle(cornerRadius: 20)
+                    RoundedRectangle(cornerRadius: 20)
+                }
+                RoundedRectangle(cornerRadius: 20)
+            }
+            .frame(maxWidth: 215)
         }
+        .foregroundColor(.white)
+        .frame(width: 700, height: 400)
         .padding()
     }
 }
